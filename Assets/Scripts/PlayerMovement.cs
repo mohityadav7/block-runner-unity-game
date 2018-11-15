34b5e477e3107cc76a5f648d84d14d10c8da96c1
Mohit Yadav <mohityadav1921@gmail.com>
2018-11-15 22:43:17 +0530

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float forwardForce = 800f;
+	public float sidewaysForce = 800f;
+
+	// Use this for initialization
+	void Start () {
+		rb.useGravity = true;
+	}
+
+	// Fixed for messing up with physics
+	void FixedUpdate () {
+		// add a forward force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if( Input.GetKey(KeyCode.RightArrow) ){
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+		if( Input.GetKey(KeyCode.LeftArrow) ){
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+        // if player falls down
+        if( rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+	}
+}
